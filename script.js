1a58b963426eee773538a7955f98374e3b5674ec
sumaiya <you@example.com>
2024-07-07 05:12:56 +0600

    Gallery Page Create and Added

--- script.js
+++ script.js
@@ -98,5 +98,33 @@ $('.five_loop').owlCarousel({
         }
     }
 })
-/*For Five Slider Ends*/    
-    
+/*For Five Slider Ends*/
+/*For Gallery Sorting Starts*/
+// init Isotope
+var $grid = $('.ice_cream_gallery').isotope({
+    itemSelector: '.portfolio_item_main',
+    layoutMode: 'fitRows'
+  });
+  // filter functions
+  var filterFns = {
+    // show if name ends with -ium
+    ium: function() {
+      var name = $(this).find('.name').text();
+      return name.match( /ium$/ );
+    }
+  };
+  // bind filter button click
+  $('.filters-button-group').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[ filterValue ] || filterValue;
+    $grid.isotope({ filter: filterValue });
+  });
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
